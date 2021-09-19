3b9aa7bef460abbbc3a0806db30fc16b3211563b
milenacsilva <milenacorreasilva@usp.br>
2021-09-19 03:44:27 -0300

    tirando teste de conexão

--- receitas/views.py
+++ receitas/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse
 import pymongo
 import os
 
-my_client = pymongo.MongoClient(os.environ.get('MONGO_CLIENT'))
-colec = my_client['onboarding']['receitas']
+# Teste para conectar na database
+# my_client = pymongo.MongoClient(os.environ.get('MONGO_CLIENT'))
+# colec = my_client['onboarding']['receitas']
 
-print(colec.find_one({}))
+# print(colec.find_one({}))
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
