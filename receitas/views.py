c120c6a977d300d5d34d6f304fb41956fce7bc2f
milenacsilva <milenacorreasilva@usp.br>
2021-09-19 03:28:48 -0300

    ajustando arquivos para deploy heroku

--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 import pymongo
-import os
+# import os
 
-my_client = pymongo.MongoClient(os.getenv('ONBOARDING_DB'))
-colec = my_client['onboarding']['receitas']
+# my_client = pymongo.MongoClient(os.getenv('ONBOARDING_DB'))
+# colec = my_client['onboarding']['receitas']
 
-print(colec.find_one({}))
+# print(colec.find_one({}))
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
