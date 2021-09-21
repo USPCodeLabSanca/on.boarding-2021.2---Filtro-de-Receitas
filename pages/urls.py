6602fec4d2f6875247f1bcf44e4e3cbb48866d83
milenacsilva <milenacorreasilva@usp.br>
2021-09-21 16:41:34 -0300

    adding templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import index_view, recipe_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', index_view, name='index'),
+    path('recipe/', recipe_view, name='recipe'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,30 @@
-from django.http import HttpResponse
-import pymongo
-import os
+from django.shortcuts import render
+from recipes.models import Recipe
 
-# Teste para conectar na database
-# my_client = pymongo.MongoClient(os.environ.get('MONGO_CLIENT'))
-# colec = my_client['onboarding']['receitas']
+def index_view(request):
+    return render(request, 'index.html', {})
 
-# print(colec.find_one({}))
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+def recipe_view(request):
+    recipe = Recipe.objects.all()[:1][0]
+    
+    context = {
+        "_id" : recipe._id,
+        "author" : recipe.author,
+        "cook_time_minutes" : recipe.cook_time_minutes,
+        "description" : recipe.description,
+        "error" : recipe.error,
+        "footnotes" : recipe.footnotes,
+        "ingredients" : recipe.ingredients,
+        "instructions" : recipe.instructions,
+        "photo_url" : recipe.photo_url,
+        "prep_time_minutes" : recipe.prep_time_minutes,
+        "rating_stars" : recipe.rating_stars,
+        "review_count" : recipe.review_count,
+        "time_scraped" : recipe.time_scraped,
+        "title": recipe.title,
+        "total_time_minutes" : recipe.total_time_minutes,
+        "url": recipe.url,
+        "keywords": recipe.keywords,
+    }
+    
+    return render(request, 'recipe.html', context)
