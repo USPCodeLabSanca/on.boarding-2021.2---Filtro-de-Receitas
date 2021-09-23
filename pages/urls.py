0afa6b4861ddc729ddb1f53507c42626cdc8f09b
GKuabara <gabrielalveskuabara@gmail.com>
2021-09-23 18:48:44 -0300

    doing filter and ajusting some other pages to sort by recipes ratings

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index_view, page_view, search_page_view
+from .views import index_view, page_view, rating_page_view, search_page_view, search_with_filter_page_view, time_page_view
 from recipes.views import recipe_detail_view
 
 urlpatterns = [
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', index_view, name='index'),
     path('pages/<int:page_num>', page_view, name='page'),
     path('recipe/<str:id>/', recipe_detail_view, name='recipe'),
-
-    # dont know if it's correct
-    path('search/<int:page_num>&&<str:ingredients>/', search_page_view, name='search'),
-
+    path('search/<str:ingredients>&&<int:page_num>/', search_page_view, name='search'),
+    path('filter/time/<int:limit_time>&&<int:page_num>/', time_page_view, name='time_filter'),
+    path('popular/<int:page_num>/', rating_page_view, name='rating_filter'),
+    path('search/filter/<int:limit_time>&&<str:ingredients>&&<int:page_num>/', search_with_filter_page_view, name='search_with_filter'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.db.models.query import QuerySet
 from django.db.models.query_utils import Q
+from django.http import request
 import recipes
 from django.shortcuts import render
 from recipes.models import Recipe
@@ -28,13 +29,63 @@ def page_view(request, page_num):
 
     return render(request, 'index.html', context)
 
-# dont ready yet
 def search_page_view(request, page_num, ingredients):
-    ingredients_list = ingredients.split()
-    queryset = []
+    ingredients = ingredients.strip()
+    if len(ingredients) == 0:
+        return page_view(request, page_num)
 
-    # not sure about this, my friends !!!
-    queryset += Recipe.objects.filter(reduce(or_, [Q(keywords__contains=x) for x in ingredients_list]))
+    ingredients_list = ingredients.split()
+
+    queryset = Recipe.objects.filter(reduce(or_, [Q(keywords__contains=x) for x in ingredients_list]))\
+             .order_by('-rating_stars')
+
+    queryset = queryset[(page_num * ITEMS_PER_PAGE):((page_num + 1)*ITEMS_PER_PAGE)]
+
+    context =  {
+        "recipe_list":queryset,
+        "page_indexes": [x for x in range(page_num - 1 if page_num > 0 else page_num, page_num + 4)]
+    }
+
+    return render(request, 'index.html', context)
+
+def time_page_view(request, page_num, limit_time):
+    if limit_time == 0:
+        return page_view(request, page_num)
+        
+    queryset = Recipe.objects.filter(total_time_minutes__lte=limit_time)\
+                .order_by('-rating_stars')\
+                [(page_num * ITEMS_PER_PAGE):((page_num + 1)*ITEMS_PER_PAGE)]
+
+    context =  {
+        "recipe_list": queryset,
+        "page_indexes": [x for x in range(page_num - 1 if page_num > 0 else page_num, page_num + 4)]
+    }
+    return render(request, 'index.html', context)
+
+def rating_page_view(request, page_num):
+    queryset = Recipe.objects.order_by('-rating_stars')\
+                [(page_num * ITEMS_PER_PAGE):((page_num + 1)*ITEMS_PER_PAGE)]
+
+    context =  {
+        "recipe_list": queryset,
+        "page_indexes": [x for x in range(page_num - 1 if page_num > 0 else page_num, page_num + 4)]
+    }
+    return render(request, 'index.html', context)
+
+def search_with_filter_page_view(request, page_num, ingredients, limit_time):
+    ingredients = ingredients.strip()
+
+    if len(ingredients) == 0:
+        return time_page_view(request, page_num, limit_time)
+    
+    if limit_time == 0:
+        return search_page_view(request, page_num, ingredients)
+    
+    ingredients_list = ingredients.split()
+
+    queryset = Recipe.objects.filter(total_time_minutes__lte=limit_time)\
+                .filter(reduce(or_, [Q(keywords__contains=x) for x in ingredients_list]))\
+                .order_by('-rating_stars')
 
     queryset = queryset[(page_num * ITEMS_PER_PAGE):((page_num + 1)*ITEMS_PER_PAGE)]
 
