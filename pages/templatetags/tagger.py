855cbf8c69fa71f770c8c5c90a9ee40269cb1527
milenacsilva <milenacorreasilva@usp.br>
2021-09-25 20:10:17 -0300

    adicionando mais informações no recipe.html

--- pages/templatetags/tagger.py
+++ pages/templatetags/tagger.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='private')
 def private(obj, attribute):
     return getattr(obj, attribute)
+
+@register.simple_tag
+def update_variable(value):
+    """Allows to update existing variable in template"""
+    return value
